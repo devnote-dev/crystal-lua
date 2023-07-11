8c3ebadfd9538642ad1bb7366f84f3a679d48531
devnote-dev <devnote.dev75@gmail.com>
2023-07-11 04:35:44 +0100

    feat: implement objects & reference

---
+++ src/crystal-lua/objects.cr
@@ -0,0 +1,47 @@
+module Lua
+  abstract class Object
+    @stack : Stack
+    getter ref : Int32?
+
+    def initialize(@stack : Stack, @ref : Int32?)
+    end
+
+    protected def preload(stack : Stack = @stack, & : Int32 ->)
+      raise "Object does not have a reference in the registry" if @ref.nil? || @ref < 1
+
+      copy_to_stack stack
+      yield stack.size
+    ensure
+      stack.remove
+    end
+
+    protected def copy_to_stack(stack : Stack = @stack)
+      raise "Object does not have a reference in the registry" if @ref.nil? || @ref < 1
+
+      stack.rawgeti @ref.as(Int32)
+    end
+
+    def release(stack : Stack = @stack)
+      if !stack.closed? && (ref = @ref)
+        stack.unref ref
+        ref = nil
+      end
+    end
+  end
+
+  class Reference
+    @stack : Stack
+    getter ref : Void*
+
+    def initialize(@stack : Stack, @ref : Void*)
+    end
+  end
+
+  class Function < Object
+    def call(*args)
+      preload do |pos|
+        @stack.call_and_return pos, *args
+      end
+    end
+  end
+end

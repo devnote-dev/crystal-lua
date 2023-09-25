3ff285997ea6a9165d97ccdb20aa199e65dc0c74
devnote-dev <devnote.dev75@gmail.com>
2023-09-25 19:33:43 +0100

    feat: implement initial state interface

---
+++ src/crystal-lua/state.cr
@@ -0,0 +1,60 @@
+module Lua
+  @[Flags]
+  enum Library
+    Base
+    Coroutine
+    Table
+    IO
+    OS
+    String
+    UTF8
+    Math
+    Debug
+    Package
+  end
+
+  class State
+    @state : LibLua::State
+    getter library : Library
+    getter? closed : Bool
+
+    def initialize
+      # TODO: should Lua be hooked up to Crystal's GC?
+      #
+      # alloc = ->(data : Void*, ptr : Void*, osize : LibC::SizeT, nsize : LibC::SizeT) do
+      #   Pointer(Void).null
+      # end
+      # @state = LibLua.newstate(alloc, alloc.pointer)
+
+      @state = LibLua.newstate
+      @library = :none
+      @closed = false
+    end
+
+    def open_library(libs : Library) : Nil
+      {% for name in %i(base coroutine table io os string utf8 math debug package) %}
+        if libs.{{ name.id }}? && !@library.{{ name.id }}?
+          unless (code = LibLua.open_{{ name.id }}(@state)) == 0
+            raise Error.from_status(code, "Failed to open library: {{ name.id }}")
+          end
+          @library &= {{ name }}
+        end
+      {% end %}
+    end
+
+    def close : Nil
+      return if @closed
+
+      LibLua.close @state
+      @closed = true
+    end
+
+    def finalize
+      close
+    end
+
+    def to_unsafe
+      @state
+    end
+  end
+end

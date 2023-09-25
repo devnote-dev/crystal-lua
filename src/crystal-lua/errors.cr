5c1861384a62cff3abde15f754b0c54444d52751
devnote-dev <devnote.dev75@gmail.com>
2023-09-25 19:33:16 +0100

    feat: add derived constructor

--- src/crystal-lua/errors.cr
+++ src/crystal-lua/errors.cr
@@ -1,5 +1,19 @@
 module Lua
   class Error < Exception
+    def self.from_status(code : Int32, message : String)
+      case LibLua::StatusCode.new(code)
+      when .errrun?
+        RuntimeError.new message
+      when .errsyntax?
+        SyntaxError.new message
+      when .errmem?
+        MemoryError.new message
+      when .errfile?
+        FileError.new message
+      else
+        FailError.new message
+      end
+    end
   end
 
   class FailError < Error

626c51acba2e390619c14417a71f05a54f33f9dd
devnote-dev <devnote.dev75@gmail.com>
2023-09-27 19:19:59 +0100

    fix(state): make aux methods functional with lib

--- src/crystal-lua/state/auxilliary.cr
+++ src/crystal-lua/state/auxilliary.cr
@@ -1,42 +1,39 @@
 module Lua
   class State
-    def load_buffer(buf : IO, mode : Mode? = nil) : Array(Any)
+    def load_buffer(buf : IO, mode : Mode? = nil) : Nil
       code = LibLua.l_loadbufferx(@state, buf.gets_to_end, buf.size, nil, mode.try &.to_s)
       raise Error.from_status(code) unless code == 0
-
-      call_and_return size
     end
 
-    def load_file(path : Path | String, mode : Mode? = nil) : Array(Any)
+    def load_file(path : Path | String, mode : Mode? = nil) : Nil
       code = LibLua.l_loadfilex(@state, path.to_s, mode.try &.to_s)
       raise Error.from_status(code) unless code == 0
-
-      call_and_return size
     end
 
-    def load_string(source : String) : Array(Any)
+    def load_string(source : String) : Nil
       code = LibLua.l_loadstring(@state, source)
       raise Error.from_status(code) unless code == 0
-
-      call_and_return size
     end
 
-    def call_and_return(pos : Int32, args : Enumerable(Any::Type) = %w[]) : Array(Any)
-      args.each { |arg| push arg }
-      code = LibLua.pcallk(@state, args.size, 1, 0, 0, nil)
+    def protected_call(num_args : Int32, num_results : Int32, msg_handler : Int32) : Nil
+      code = LibLua.pcallk(@state, num_args, num_results, msg_handler, 0, nil)
       raise Error.from_status(code) unless code == 0
+    end
 
-      (pos..size).compact_map { pop }
+    def protected_call(num_args : Int32, num_results : Int32, msg_handler : Int32,
+                       context : LibLua::KContext, fn : LibLua::KFunction) : Nil
+      code = LibLua.pcallk(@state, num_args, num_results, msg_handler, context, fn)
+      raise Error.from_status(code) unless code == 0
     end
 
     def run_file(path : Path | String) : Nil
-      code = LibLua.l_dofile(@state, path.to_s)
-      raise Error.from_status(code) unless code == 0
+      load_file path, :binary_text
+      protected_call 0, -1, 0
     end
 
     def run_string(source : String) : Nil
-      code = LibLua.l_dostring(@state, source)
-      raise Error.from_status(code) unless code == 0
+      load_string source
+      protected_call 0, -1, 0
     end
 
     def reference(pos : Int32) : Int32

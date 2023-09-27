b0fe3ce4e2c571b2aa032db1160cff7ca66c4df7
devnote-dev <devnote.dev75@gmail.com>
2023-09-27 04:34:58 +0100

    fix(state): resolve varying return types from old impl

--- src/crystal-lua/state/auxilliary.cr
+++ src/crystal-lua/state/auxilliary.cr
@@ -1,37 +1,32 @@
 module Lua
   class State
-    def load_buffer(buf : IO, mode : Mode? = nil)
+    def load_buffer(buf : IO, mode : Mode? = nil) : Array(Any)
       code = LibLua.l_loadbufferx(@state, buf.gets_to_end, buf.size, nil, mode.try &.to_s)
       raise Error.from_status(code) unless code == 0
 
       call_and_return size
     end
 
-    def load_file(path : Path | String, mode : Mode? = nil)
+    def load_file(path : Path | String, mode : Mode? = nil) : Array(Any)
       code = LibLua.l_loadfilex(@state, path.to_s, mode.try &.to_s)
       raise Error.from_status(code) unless code == 0
 
       call_and_return size
     end
 
-    def load_string(source : String)
+    def load_string(source : String) : Array(Any)
       code = LibLua.l_loadstring(@state, source)
       raise Error.from_status(code) unless code == 0
 
       call_and_return size
     end
 
-    private def call_and_return(pos : Int32, *args)
+    def call_and_return(pos : Int32, args : Enumerable(Any::Type) = %w[]) : Array(Any)
       args.each { |arg| push arg }
-      code = LibLua.pcallk(@state, args.size, -1, 0, nil)
+      code = LibLua.pcallk(@state, args.size, 1, 0, 0, nil)
       raise Error.from_status(code) unless code == 0
 
-      elements = (pos..size).map { pop }
-      if elements.one?
-        elements
-      else
-        elements[0]
-      end
+      (pos..size).compact_map { pop }
     end
 
     def run_file(path : Path | String) : Nil

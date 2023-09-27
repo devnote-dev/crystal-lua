a218937c553f4b7541ebd692a1e4752f19afa72d
devnote-dev <devnote.dev75@gmail.com>
2023-09-27 22:55:50 +0100

    refactor: object -> base restructure

--- src/crystal-lua/base.cr
+++ src/crystal-lua/base.cr
@@ -1,5 +1,36 @@
 module Lua
-  class Table < Lua::Object
+  private class Base
+    @state : State
+    getter! ref : Int32
+
+    def initialize(@state : State, @ref : Int32?)
+    end
+
+    protected def preload(state : State = @state, & : Int32 -> _)
+      ensure_reference!
+      LibLua.rawgeti(state, -1_001_000, ref)
+      yield state.size
+    ensure
+      state.remove
+    end
+
+    protected def ensure_reference! : Nil
+      return if ref? && ref > 0
+      raise RuntimeError.new "Object does not have a valid reference in the registry"
+    end
+  end
+
+  class Function < Base
+    def call(*args : Any::Type) : Any?
+      preload do |pos|
+        args.each { |arg| @state.push arg }
+        @state.protected_call args.size, 1, 0
+        @state.pop
+      end
+    end
+  end
+
+  class Table < Base
     include Enumerable({Any, Any})
 
     def each(& : {Any, Any} ->)

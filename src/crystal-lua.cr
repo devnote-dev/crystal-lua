c300a886587deb250ace64f2b994c90892e1755d
devnote-dev <devnote.dev75@gmail.com>
2024-09-07 13:36:41 +0100

    feat: general code cleanup

--- src/crystal-lua.cr
+++ src/crystal-lua.cr
@@ -1,5 +1,10 @@
 require "./crystal-lua/*"
 
 module Lua
-  VERSION = "0.1.0"
+  VERSION     = "0.1.0"
+  LIB_VERSION = "5.4.4"
+
+  REGISTRY_INDEX = -1_001_000
+  ENV_INDEX      = -1_001_001
+  GLOBALS_INDEX  = -1_001_002
 end
--- src/crystal-lua/export.cr
+++ src/crystal-lua/export.cr
@@ -4,7 +4,7 @@ module Lua
 
   macro export_function(state, name, &block)
     {% name.raise "expected argument #2 to be a string literal" unless name.is_a?(StringLiteral) %}
-    proc = ->(ptr : LibLua::State) do
+    %proc = ->(ptr : LibLua::State) do
       %state = Lua::State.new ptr
       {% unless block.args.empty? %}
         {% for arg, index in block.args %}
